a5379344fc98e64c412e4a1354f94b7b75cb4152
daxmictar <dev@daxmictar.net>
2026-03-13 03:44:14 -0700

    added recipe book lab extra credit

--- RecipeBook/app/src/main/java/com/example/recipebook/MainActivity.kt
+++ RecipeBook/app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -11,9 +11,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,7 +75,19 @@ data class Recipe(
 }
 
 object RecipeCollection {
+    var recipeNumber: Int = 1
+
     val recipes = mutableListOf<Recipe>()
+
+    fun addRecipe(name: String, ingredients: List<String>, instructions: String) {
+        recipes.add(Recipe(
+            id = recipeNumber++,
+            name = name,
+            ingredients = ingredients,
+            instructions = instructions
+        ))
+    }
+
     fun getById(id: Int) : Recipe {
         return recipes.first {
             recipe -> recipe.id == id
@@ -110,37 +128,84 @@ fun RecipeDetailScreen(recipe: Recipe, onBackClick: () -> Unit) {
 }
 
 @Composable
-fun RecipeListScreen(onRecipeClick: (Int) -> Unit) {
-    // RecipeCollection.ComposeRecipes()
+fun RecipeAddScreen(onBackClick: () -> Unit) {
+    var recipeName by remember { mutableStateOf("") }
+    var recipeIngredients by remember { mutableStateOf("") }
+    var recipeInstructions by remember { mutableStateOf("") }
+    var modifier = Modifier.fillMaxWidth()
+
+    Column(
+        modifier = modifier
+    ) {
+        Button(onClick = onBackClick) {
+            Text(text = "Back")
+        }
+        OutlinedTextField(
+            modifier = modifier,
+            value = recipeName,
+            onValueChange = { recipeName = it },
+            label = { Text(text = "Enter a recipe name") }
+        )
+        OutlinedTextField(
+            modifier = modifier,
+            value = recipeIngredients,
+            onValueChange = { recipeIngredients = it },
+            label = { Text(text = "Enter ingredients, each on a new line.") }
+        )
+        OutlinedTextField(
+            modifier = modifier,
+            value = recipeInstructions,
+            onValueChange = { recipeInstructions = it },
+            label = { Text(text = "Enter instructions") }
+        )
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = {
+               RecipeCollection.addRecipe(
+                   name = recipeName,
+                   ingredients = recipeIngredients.split("\n"),
+                   instructions = recipeInstructions
+               )
+
+               onBackClick()
+            }
+        ) {
+            Text("Add Recipe")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun RecipeAddScreenPreview() {
+    RecipeAddScreen({})
 }
 
 object Destinations {
+    const val RECIPE_ADD_SCREEN = "recipe_add_screen"
     const val RECIPE_LIST_SCREEN = "recipe_list_screen"
     const val RECIPE_DETAIL_SCREEN = "recipe_detail_screen/{recipeId}"
     fun recipeDetailsScreen(recipeId: Int): String { return "recipe_detail_screen/$recipeId" }
 }
-
-val recipes = listOf(
-    Recipe(
-        id=1,
-        name = "Fried Egg",
-        ingredients = listOf("1 egg", "1 tsp butter"),
-        instructions = "Butter pan lightly, cook egg to desired consistency"
-    ),
-    Recipe(
-        id=2,
-        name = "Pico de Gallo",
-        ingredients = listOf("2 roma tomatoes", "1 onion", "1 jalapeno", "1/2 lime", "1/2 bunch cilantro"),
-        instructions = "Finely chop and mix ingredients, juice lime."
-    ),
-)
 
 @Preview(
     showBackground = true,
 )
 @Composable
 fun RecipeAppPreview() {
-    RecipeCollection.recipes.addAll(recipes)
+    // default recipes
+    RecipeCollection.addRecipe(
+        name = "Fried Egg",
+        ingredients = listOf("1 egg", "1 tsp butter"),
+        instructions = "Butter pan lightly, cook egg to desired consistency"
+    )
+    RecipeCollection.addRecipe(
+        name = "Pico de Gallo",
+        ingredients = listOf("2 roma tomatoes", "1 onion", "1 jalapeno", "1/2 lime", "1/2 bunch cilantro"),
+        instructions = "Finely chop and mix ingredients, juice lime."
+    )
 
     val recipeNavController = rememberNavController()
     NavHost(
@@ -148,19 +213,22 @@ fun RecipeAppPreview() {
         startDestination = Destinations.RECIPE_LIST_SCREEN
     ) {
         composable(route = Destinations.RECIPE_LIST_SCREEN) {
-            RecipeCollection.ComposeRecipes(
-                onRecipeClick = {
-                    recipeId -> recipeNavController.navigate(Destinations.recipeDetailsScreen(recipeId))
-                }
-            )
+            Column {
+                RecipeCollection.ComposeRecipes(
+                    onRecipeClick = { recipeId ->
+                        recipeNavController.navigate(Destinations.recipeDetailsScreen(recipeId))
+                    }
+                )
 
-            // Experiment:
-            //RecipeListScreen(
-            //    onRecipeClick = {
-            //        recipeId ->
-            //            recipeNavController.navigate(Destinations.RECIPE_DETAIL_SCREEN)
-            //    }
-            //)
+                Button(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    onClick = {
+                        recipeNavController.navigate(Destinations.RECIPE_ADD_SCREEN)
+                    }
+                ) {
+                    Text("Add New Recipe")
+                }
+            }
         }
 
         composable(
@@ -175,12 +243,25 @@ fun RecipeAppPreview() {
                 val recipeId = backStackEntry.arguments?.getInt("recipeId")
                     ?: return@composable
 
-            RecipeDetailScreen(
-                recipe = RecipeCollection.getById(recipeId),
-                onBackClick = {
-                    recipeNavController.popBackStack()
-                }
-            )
+            Column {
+                RecipeDetailScreen(
+                    recipe = RecipeCollection.getById(recipeId),
+                    onBackClick = {
+                        recipeNavController.popBackStack()
+                    }
+                )
+
+            }
+        }
+
+        composable(
+            route = Destinations.RECIPE_ADD_SCREEN
+        ) {
+           RecipeAddScreen(
+               onBackClick = {
+                   recipeNavController.popBackStack()
+               },
+           )
         }
     }
 }
